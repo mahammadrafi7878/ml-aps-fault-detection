1647423f7967cf6575c89ae8aa31407c46d3e70f
mahammad <mahammadrafishaik222@gmail.com>
2022-12-05 18:14:19 +0530

    data ingestion completed

--- sensor/utils/__init__.py
+++ sensor/utils/__init__.py
@@ -3,16 +3,29 @@ from sensor.config import mongo_client
 from sensor.logger import logging
 from sensor.exception import SensorException
 import os,sys
-def get_collection_as_dataframe(database_name:str,collection_name:str):
-    try: 
-        logging.info(f"Reading from database:{database_name}and collection:{collection_name}")
-        df=pd.DataFrame(list(mongo_client[database_name][collection_name].find()))
-        logging.info(f"found columns:{df.columns}")
-        if "_id" in df.columns:
-            logging.info(f"droping column:_id")
-            df=df.drop("_id",axis=1)
-        logging.info(f"rows and columns {df.shape}")
-        return df
 
+
+
+
+
+def get_collection_as_dataframe(database_name:str,  collection_name:str)->pd.DataFrame:
+    """
+    Description: This function return collection as dataframe
+    =========================================================
+    Params:
+    database_name: database name
+    collection_name: collection name
+    =========================================================
+    return Pandas dataframe of a collection
+    """
+    try:
+        logging.info(f"Reading data from database: {database_name} and collection: {collection_name}")
+        df = pd.DataFrame(list(mongo_client[database_name][collection_name].find()))
+        logging.info(f"Found columns: {df.columns}")
+        if "_id" in df.columns:
+            logging.info(f"Dropping column: _id ")
+            df = df.drop("_id",axis=1)
+        logging.info(f"Row and columns in df: {df.shape}")
+        return df
     except Exception as e:
-        raise SensorException(e,sys)
+        raise SensorException(e, sys)
